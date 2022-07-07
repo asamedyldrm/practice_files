743fb59af975e1f21828e0a51527a5e82207300b
Samet Yıldırım <smtyldrm32@gmail.com>
2022-07-07 19:21:25 +0300

    if-elif-else

---
+++ if_else/if-elif-else.py
@@ -0,0 +1,21 @@
+# yaş = int(input("Yaşınız: "))
+# if yaş == 18:
+#     print("18 iyidir!")
+# if yaş < 0:
+#     print("Yok canım, daha neler!...")
+# if yaş < 18:
+#     print("Genç bir kardeşimizsin!")
+# if yaş > 18:
+#     print("Eh, artık yaş yavaş yavaş kemale eriyor!")
+
+soru = input("Bir meyve adı söyleyin bana:")
+if soru == "elma":
+    print("evet, elma bir meyvedir...")
+elif soru == "karpuz":
+    print("evet, karpuz bir meyvedir...")
+elif soru == "armut":
+    print("evet, armut bir meyvedir...")
+else:
+    print(soru, "gerçekten bir meyve midir?")
+
+
