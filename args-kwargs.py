1938486365acdd78519dd8eb4c118b5e6820d718
Samet Yıldırım <106842297+samettyldrm@users.noreply.github.com>
2022-07-11 05:31:25 +0300

    Update args-kwargs.py

--- args-kwargs.py
+++ args-kwargs.py
@@ -1,47 +1,47 @@
-# def toplama(a,b):
-#     return a+b
-#
-# print(toplama(3,4))
+def toplama(a,b):
+    return a+b
 
-# def toplama(*args):
-#     toplam = 0
-#     for i in args:
-#         toplam += i
-#     return toplam
-# print(top4,5))
+print(toplama(3,4))
 
-# def topla(*args):
-#     print(type(args))
-#
-# topla(1,2,3,4,5)
+def toplama(*args):
+    toplam = 0
+    for i in args:
+        toplam += i
+    return toplam
+print(top4,5))
 
-# def topla(p, *args):
-#     print(p)
-#     print(args)
-#
-#
-# topla(1, 2, 3, 4, 5)
-#
-#
-# def topla(*args, p):
-#     print(p)
-#     print(args)
+def topla(*args):
+    print(type(args))
+
+topla(1,2,3,4,5)
+
+def topla(p, *args):
+    print(p)
+    print(args)
 
 
-# topla(1, 2, 3, 4, p=5)  # burada p=5 ifadesi verirsek hata almayız.
+topla(1, 2, 3, 4, 5)
 
 
-# def yazdir(**kwargs):
-#     print(kwargs)
-#
-#
-# yazdir(ad="Ahmet", soyad="Yılmaz")
+def topla(*args, p):
+    print(p)
+    print(args)
 
-# def yazdir(**kwargs):
-#     for k in kwargs:
-#         print(k, kwargs[k])
-#
-# yazdir(ad="Ahmet", soyad="Yılmaz", yaş= 25)
+
+topla(1, 2, 3, 4, p=5)  # burada p=5 ifadesi verirsek hata almayız.
+
+
+def yazdir(**kwargs):
+    print(kwargs)
+
+
+yazdir(ad="Ahmet", soyad="Yılmaz")
+
+def yazdir(**kwargs):
+    for k in kwargs:
+        print(k, kwargs[k])
+
+yazdir(ad="Ahmet", soyad="Yılmaz", yaş= 25)
 
 def yazdir(*args, **kwargs):
     print(sum(args))
